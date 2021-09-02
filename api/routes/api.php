3d8dde77dee9d2973e4892589bbd3ccbe3bbdcdd
yuri2397 <diawlemor97@gmail.com>
2021-09-02 12:36:18 +0000

    make it better

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\Admin\Auth\AuthController;
-use App\Http\Controllers\CompteController;
-use App\Http\Controllers\EtudiantController;
-use App\Http\Controllers\RestoController;
-use App\Http\Controllers\VigilController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestoController;
+use App\Http\Controllers\VigilController;
+use App\Http\Controllers\CompteController;
+use App\Http\Controllers\PayTechController;
+use App\Http\Controllers\EtudiantController;
+use App\Http\Controllers\Admin\Auth\AuthController;
 
 /**
  * AUTH ROUTES
@@ -48,18 +49,17 @@ Route::prefix('etudiant')->middleware(['auth:api'])->group(function () {
     Route::get('/payments', [RestoController::class, 'userPayments']);
 
     Route::post('/edit-password', [EtudiantController::class, 'editPassword']);
-	Route::post('/edit-pin', [CompteController::class, 'editPin']);
+    Route::post('/edit-pin', [CompteController::class, 'editPin']);
     Route::post('/reset-pin', [CompteController::class, 'resetPin'])->withoutMiddleware('auth:api');
 
-	// demande de réinitialisation de mot de passe
+    // demande de réinitialisation de mot de passe
     Route::post('/reset-password', [EtudiantController::class, 'resetPassword'])->withoutMiddleware('auth:api');
     Route::post('/new-password', [EtudiantController::class, 'newPassword'])->withoutMiddleware('auth:api');
-
 });
 
 Route::prefix('vigil')->middleware(['auth:controller'])->group(function () {
     Route::post('/register', [VigilController::class, 'store'])->withoutMiddleware('auth:controller');
-	Route::post('/login', [VigilController::class, 'login'])->withoutMiddleware('auth:controller');
+    Route::post('/login', [VigilController::class, 'login'])->withoutMiddleware('auth:controller');
     Route::post('/logout', [VigilController::class, 'logout']);
 
     Route::get('/', [VigilController::class, 'user']);
@@ -67,3 +67,11 @@ Route::prefix('vigil')->middleware(['auth:controller'])->group(function () {
 });
 
 Route::get('tarifs',  [RestoController::class, 'tarifs'])->middleware(['auth:controller']);
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('/payment', [PayTechController::class, 'payment']);
+
+    Route::post('/pay-ipn', [PayTechController::class, 'ipn'])->withoutMiddleware('auth:api');
+    Route::get('/pay-cancel', [PayTechController::class, 'cancel'])->withoutMiddleware('auth:api');
+    Route::get('/pay-success', [PayTechController::class, 'succss'])->withoutMiddleware('auth:api');
+});
